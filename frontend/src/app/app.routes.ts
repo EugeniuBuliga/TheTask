4ec9e34b1edaaa97d381eb114625a825537f8d35
Eugeniu Buliga <61945477+EugeniuBuliga@users.noreply.github.com>
2025-07-15 23:44:42 +0300

    feat: removed ping test feature residual files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import {Routes} from '@angular/router';
 import {AuthGuard} from './core/auth/auth.guard';
-import {PingComponent} from './features/ping/ping.component';
 import {Login} from './core/auth/pages/login/login';
 import {LoginGuard} from './core/auth/pages/login/login.guard';
 import {Register} from './core/auth/pages/register/register';
--- frontend/src/app/features/ping/ping.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { PingComponent } from './ping.component';
-
-export const pingRoutes: Routes = [
-  {
-    path: '',
-    component: PingComponent,
-  },
-];
